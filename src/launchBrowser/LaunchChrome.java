a1447289979440dbe19a159533452c3112823288
FarzanaKamal <farzanakamal973@gmail.com>
2022-12-11 11:49:21 -0600

    First Commit

---
+++ src/launchBrowser/LaunchChrome.java
@@ -0,0 +1,27 @@
+package launchBrowser;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		//Scanner scn = new Scanner (System.in);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("q")).sendKeys("selenium");
+		driver.findElement(By.name("btnK")).click();
+		
+		Thread.sleep(3000);
+		
+		//driver.close();
+		
+		
+	}
+
+}
